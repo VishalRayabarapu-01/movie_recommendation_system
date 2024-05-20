8b93068ed040fb84af9041f1414d889e69e40acd
VishalRayabarapu-01 <130127088+VishalRayabarapu-01@users.noreply.github.com>
2024-05-20 13:03:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ docs = load_docs()
 
 new_movies = pd.read_csv('./cleaned_csv_3.csv')
 
-st.title("A Personalised Movie Recommendation System developed by Vishal ")
+st.title("Movie Recommendation System")
 st.write("Find your next favorite movie!")
 
 movie_name = st.text_input("Enter a movie name:")
